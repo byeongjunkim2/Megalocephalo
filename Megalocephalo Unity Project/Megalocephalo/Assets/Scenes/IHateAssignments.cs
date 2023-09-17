d3302217124d6dcadd085143f5161462f0e6be56
byeongjun.kim <byeongjun.kim@3f42da63-03f7-4ee7-9da0-e62120e79d6b>
2023-09-17 20:54:12 +0000

    commit from nuni
    git-svn-id: https://svn.digipen.edu/projects/flapjack/trunk@7 3f42da63-03f7-4ee7-9da0-e62120e79d6b

---
+++ Megalocephalo Unity Project/Megalocephalo/Assets/Scenes/IHateAssignments.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IHateAssignments : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("I hate assignments!!!!!!");
+    }
+}

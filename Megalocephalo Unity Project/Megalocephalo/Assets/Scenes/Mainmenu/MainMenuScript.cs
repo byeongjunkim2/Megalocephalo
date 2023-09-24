ead2b15b04bf755a2089b2937a25407c49d3f248
vincent.rikie <vincent.rikie@3f42da63-03f7-4ee7-9da0-e62120e79d6b>
2023-09-24 20:39:11 +0000

    git-svn-id: https://svn.digipen.edu/projects/flapjack/trunk@40 3f42da63-03f7-4ee7-9da0-e62120e79d6b

---
+++ Megalocephalo Unity Project/Megalocephalo/Assets/Scenes/Mainmenu/MainMenuScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Button PrototypeB;
+    public Button AudioZooB;
+    public Button QuitB;
+    void Start()
+    {
+        PrototypeB.onClick.AddListener(PlayDemo);
+        AudioZooB.onClick.AddListener(PlayAudioZoo);
+        QuitB.onClick.AddListener(Quit);
+    }
+        public void PlayDemo()
+    {
+        SceneManager.LoadScene("Prototype");
+    }
+
+    public void PlayAudioZoo()
+    {
+        SceneManager.LoadScene("Audio_Zoo");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}

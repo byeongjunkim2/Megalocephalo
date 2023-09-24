a649a85879c53a692b796b43970dc582b80cf865
byeongjun.kim <byeongjun.kim@3f42da63-03f7-4ee7-9da0-e62120e79d6b>
2023-09-24 08:09:58 +0000

    git-svn-id: https://svn.digipen.edu/projects/flapjack/trunk@37 3f42da63-03f7-4ee7-9da0-e62120e79d6b

---
+++ Megalocephalo Unity Project/Megalocephalo/Assets/wallCheckTest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class wallCheckTest : MonoBehaviour
+{
+    public Transform trs;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //collision.gameObject.transform.position = collision.gameObject.transform.position + new Vector3(0, 0, -.5f);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

fa0925285c48cf1d59ebb1ce3417b465337d6108
hyojoon.kim <hyojoon.kim@3f42da63-03f7-4ee7-9da0-e62120e79d6b>
2023-09-28 20:27:25 +0000

    changed to stop game when rotating the camera
    git-svn-id: https://svn.digipen.edu/projects/flapjack/trunk@90 3f42da63-03f7-4ee7-9da0-e62120e79d6b

--- Megalocephalo Unity Project/Megalocephalo/Assets/Scripts/TestRotate.cs
+++ Megalocephalo Unity Project/Megalocephalo/Assets/Scripts/TestRotate.cs
@@ -24,6 +24,7 @@ public class TestRotate : MonoBehaviour
         {
             targetRotationY -= 90f;
             isRotating = true;
+            Time.timeScale = 0; // pause
         }
 
         if (isRotating)
@@ -39,13 +40,14 @@ public class TestRotate : MonoBehaviour
         Quaternion currentRotation = transform.rotation;
         Quaternion targetRotation = Quaternion.Euler(0, targetRotationY, 0);
 
-        transform.rotation = Quaternion.Lerp(currentRotation, targetRotation, Time.deltaTime * rotationSpeed);
-
+        // transform.rotation = Quaternion.Lerp(currentRotation, targetRotation, Time.deltaTime * rotationSpeed);
+        transform.rotation = Quaternion.Lerp(currentRotation, targetRotation, Time.unscaledDeltaTime * rotationSpeed);
         // 
         if (Quaternion.Angle(currentRotation, targetRotation) < 1f)
         {
             isRotating = false;
             transform.rotation = targetRotation;  // 
+            Time.timeScale = 1; // stop pause
         }
     }
 
@@ -53,7 +55,32 @@ public class TestRotate : MonoBehaviour
     {
         targetRotationY += angle;
         isRotating = true;
+        Time.timeScale = 0; // pause
+
+                            // StartCoroutine(RotateCamera());
     }
+
+    IEnumerator RotateCamera()
+    {
+        isRotating = true;
+        Time.timeScale = 0; // pause
+
+        float rotatedAmount = 0;
+        float targetRotation = 90; // 회전하고자 하는 각도
+
+        while (rotatedAmount < targetRotation)
+        {
+            float rotationThisFrame = rotationSpeed * Time.unscaledDeltaTime; // 일시정지 상태에서도 정상적으로 작동하기 위해 Time.unscaledDeltaTime을 사용합니다.
+            rotatedAmount += rotationThisFrame;
+            transform.Rotate(Vector3.up, rotationThisFrame);
+
+            yield return null;
+        }
+
+        Time.timeScale = 1; // stop pause
+        isRotating = false;
+    }
+
 
 
 }

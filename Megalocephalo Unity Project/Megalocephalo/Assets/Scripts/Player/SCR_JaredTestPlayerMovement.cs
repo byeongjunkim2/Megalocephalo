3aa3d143c3744bdb122392a5dd7f1e3c9b4c8aed
jared.gardner <jared.gardner@3f42da63-03f7-4ee7-9da0-e62120e79d6b>
2023-10-15 22:15:01 +0000

    updated jared test movement to include circular movement
    git-svn-id: https://svn.digipen.edu/projects/flapjack/trunk@171 3f42da63-03f7-4ee7-9da0-e62120e79d6b

--- Megalocephalo Unity Project/Megalocephalo/Assets/Scripts/Player/SCR_JaredTestPlayerMovement.cs
+++ Megalocephalo Unity Project/Megalocephalo/Assets/Scripts/Player/SCR_JaredTestPlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 // code by jared 
@@ -8,13 +9,15 @@ public class SCR_JaredTestPlayerMovement : MonoBehaviour
 {
 
     public Rigidbody rb;
-    public float orientation; // what angle the player moves in, in degrees
     public bool facingRight; // whether or not the player is facing right; does not mean visibly
     public bool onGround; // whether or not the player is on the ground
 
     public float walkSpeed; // how fast the player walks
     public float gravityAcceleration; // how fast the player's y velocity increases as they fall
     public Vector2 moveVector; // the vector used for movement, later rotated by the orientation
+
+    public float orientation; // what angle the player moves in, in degrees
+    public float orientationSpeed; // how fast the angle changes as you walk, for circular movement
 
     public float initRotationOffset; // initial offset of player, in degrees
     public float facingRotationOffset; // how much the player rotates based on the facing direction, in degrees
@@ -52,6 +55,8 @@ public class SCR_JaredTestPlayerMovement : MonoBehaviour
 
             // set horizontal velocity
             moveVector += new Vector2(Input.GetAxisRaw("Horizontal") * walkSpeed, 0.0f);
+            orientation += Input.GetAxisRaw("Horizontal") * orientationSpeed * walkSpeed * Time.deltaTime;
+            orientation = (orientation + 360.0f) % 360.0f;
 
         }
 
@@ -67,8 +72,8 @@ public class SCR_JaredTestPlayerMovement : MonoBehaviour
         rb.velocity = Vector3.RotateTowards(movementVector, orientationVector * facing(), orientation * Mathf.Deg2Rad, 0.0f);
 
         // rotate player (visually)
-        currentRotationOffset = Mathf.Lerp(currentRotationOffset, initRotationOffset - facing() * facingRotationOffset + orientation, rotationLerpAmount);
-        Vector3 rotationVector = new Vector3(0.0f, currentRotationOffset, 0.0f);
+        currentRotationOffset = Mathf.Lerp(currentRotationOffset, initRotationOffset - facing() * facingRotationOffset, rotationLerpAmount);
+        Vector3 rotationVector = new Vector3(0.0f, currentRotationOffset + orientation, 0.0f);
         transform.rotation = Quaternion.Euler(rotationVector);
 
     }
